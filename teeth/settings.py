afe39099aff33fffcdce7b7f6240e31a3d18dee6
reza <wanted.swift57@gmail.com>
2022-11-15 21:57:39 +0330

    fuck you

--- teeth/settings.py
+++ teeth/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'teeth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR2 / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3") ,
+        
     }
 }
 
